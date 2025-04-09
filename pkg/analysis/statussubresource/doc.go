530c2fa87cd1026184366383416417cbdda14700
Joel Speed <joel.speed@hotmail.co.uk>
2025-04-09 16:21:16 +0100

    Add license header to all Go files

--- pkg/analysis/statussubresource/doc.go
+++ pkg/analysis/statussubresource/doc.go
@@ -1,10 +1,16 @@
-// statussubresource is a linter to check that the status subresource is configured correctly for
-// structs marked with the 'kubebuilder:object:root:=true' marker. Correct configuration is that
-// when there is a status field the 'kubebuilder:subresource:status' marker is present on the struct
-// OR when the 'kubebuilder:subresource:status' marker is present on the struct there is a status field.
-//
-// In the case where there is a status field present but no 'kubebuilder:subresource:status' marker, the
-// linter will suggest adding the comment '// +kubebuilder:subresource:status' above the struct.
-//
-// This linter is not enabled by default as it is only applicable to CustomResourceDefinitions.
+/*
+Copyright 2025 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package statussubresource
