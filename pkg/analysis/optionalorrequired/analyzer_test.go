3472567c8a4d94decf08f5c2670feea243d6c708
sivchari <shibuuuu5@gmail.com>
2025-04-11 19:45:29 +0900

    check if type spec has optional or required marker
    Signed-off-by: sivchari <shibuuuu5@gmail.com>

--- pkg/analysis/optionalorrequired/analyzer_test.go
+++ pkg/analysis/optionalorrequired/analyzer_test.go
@@ -49,3 +49,19 @@ func TestSwappedMarkerPriority(t *testing.T) {
 
 	analysistest.RunWithSuggestedFixes(t, testdata, a, "b")
 }
+
+func TestTypeSpec(t *testing.T) {
+	testdata := analysistest.TestData()
+
+	a, err := optionalorrequired.Initializer().Init(config.LintersConfig{
+		OptionalOrRequired: config.OptionalOrRequiredConfig{
+			PreferredOptionalMarker: optionalorrequired.KubebuilderOptionalMarker,
+			PreferredRequiredMarker: optionalorrequired.KubebuilderRequiredMarker,
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	analysistest.RunWithSuggestedFixes(t, testdata, a, "c")
+}
